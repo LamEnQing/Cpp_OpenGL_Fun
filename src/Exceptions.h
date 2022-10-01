1a17f51c09ac6c2d456cd52be80a407cb3ab28ef
LamEnQing <enqinglam8@gmail.com>
2022-10-01 17:18:18 +0800

    Improved JSON exception message

--- src/Exceptions.h
+++ src/Exceptions.h
@@ -17,12 +17,19 @@ namespace OpenGLFun {
 	// To simplify writing the message string for json read errors
 	class JsonReadException : public SimpleException {
 	public:
+		/**
+		 * @brief Takes in 4 parameters
+		 * @param filename The filename of the json parsing, or the parent property
+		 * @param propertyOwner Parent property of the property you are parsing
+		 * @param propertyName The key of the property you are parsing
+		 * @param propertyJsonType The value type of the property you are parsing
+		*/
 		JsonReadException(std::string filename, std::string propertyOwner, std::string propertyName, std::string propertyJsonType) : SimpleException("In ") {
-			_message += filename + ", " + propertyOwner + "'s \"" + propertyName + "\" must be a " + propertyJsonType;
+			_message += filename + ", " + propertyOwner + "'s \"" + propertyName + "\" must have a value of a" + propertyJsonType;
 		}
 
 		JsonReadException(std::string filename, std::string propertyName, std::string propertyJsonType) : SimpleException("In ") {
-			_message += filename + ", \"" + propertyName + "\" must be a " + propertyJsonType;
+			_message += filename + ", \"" + propertyName + "\" must have a value of a" + propertyJsonType;
 		}
 	};
 }
