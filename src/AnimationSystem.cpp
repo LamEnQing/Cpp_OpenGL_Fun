cac90b7f0b99b2b72d871c3b262cce83b0def00d
LamEnQing <enqinglam8@gmail.com>
2022-10-09 10:39:01 +0800

    AnimationSystem works, in a hardcoded way

--- src/AnimationSystem.cpp
+++ src/AnimationSystem.cpp
@@ -1,10 +1,41 @@
 #include "AnimationSystem.h"
 
+#include "ComponentManager.h"
+#include "EntityManager.h"
+#include "ModelComponent.h"
+#include "ResourceManager.h"
+#include "Transform.h"
+
 namespace OpenGLFun {
 	AnimationSystem::AnimationSystem() {}
 	AnimationSystem::~AnimationSystem() {}
 
 	void AnimationSystem::Update(float const& deltaTime) {
+		for (EntityId const& entityId : ENTITY_MANAGER->GetEntities()) {
+			if (!COMPONENT_MANAGER->HasComponent(entityId, ComponentType::Transform) || !COMPONENT_MANAGER->HasComponent(entityId, ComponentType::Model))
+				continue;
 
+			ModelComponent* modelComp = COMPONENT_MANAGER->GetComponent<ModelComponent>(entityId, ComponentType::Model);
+			if (modelComp->mModelType == ModelType::TwoD) continue;
+			if (modelComp->mModelFilepath.empty()) continue;
+
+			Transform* transform = COMPONENT_MANAGER->GetComponent<Transform>(entityId, ComponentType::Transform);
+			Model* model = RESOURCE_MANAGER->GetModel(modelComp->mModelFilepath);
+
+			for (auto meshesIt = model->GetMeshMap().begin(); meshesIt != model->GetMeshMap().end(); meshesIt++) {
+				std::string const& meshName = meshesIt->first;
+				if (meshName.find("leg") != std::string::npos && meshName.find("left") != std::string::npos) {
+					Mesh* mesh = meshesIt->second.get();
+
+					Vec3f rotation = mesh->GetRotation();
+					rotation.z += 20 * deltaTime;
+
+					if (rotation.z > 360)
+						rotation.z = rotation.z - 360;
+
+					mesh->SetRotation(rotation);
+				}
+			}
+		}
 	}
 }
