6d8fd9c4028bdaa945a472934a27526e89319467
LamEnQing <enqinglam8@gmail.com>
2022-11-06 13:50:00 +0800

    Weird bug, but anyway, enabled console in debug mode

--- Cpp_OpenGL_Fun/src/main.cpp
+++ Cpp_OpenGL_Fun/src/main.cpp
@@ -1,16 +1,18 @@
 #define _CRTDBG_MAP_ALLOC
-#include <stdlib.h>
 #include <crtdbg.h>
+#include <stdlib.h>
+#include <Windows.h>
 #include "Engine.h"
 
 namespace OpenGLFun {
 	Engine* ENGINE = nullptr;
 }
 
-int main(int argc, char* argv[]) {
+int main() {
 #ifdef _DEBUG
 	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
 	//_CrtSetBreakAlloc(217);
+	::ShowWindow(::GetConsoleWindow(), true);
 #endif
 
 	try {
